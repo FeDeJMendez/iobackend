45e4ab1c17080c40591d4894cdff826dd7498134
FeDeJMendez <fedemendezg@gmail.com>
2022-10-25 17:31:33 -0300

    Init

---
+++ src/main/java/com/invop/iobackend/IobackendApplication.java
@@ -0,0 +1,13 @@
+package com.invop.iobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invop/iobackend/IobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invop.iobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
